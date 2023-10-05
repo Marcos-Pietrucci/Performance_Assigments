a07186db9fe0d3cb8fc2573bdb5dd6eee472e606
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-10-05 18:48:56 +0200

    First statistical values calculated

--- A03/A03.m
+++ A03/A03.m
@@ -2,9 +2,33 @@
 % Assigment 3
 
 %Reading the Traces
-    %Just like in class, will add it to a 3 column matrix
-fl1 = csvread('Trace1.csv');
-fl2 = csvread('Trace2.csv');
-fl3 = csvread('Trace3.csv');
+DataSet = csvread('Trace3.csv');
+%DataSet = csvread('Trace2.csv');
+%DataSet = csvread('Trace3.csv');
 
-DataSet = [fl1, fl2, fl2];
+%Creating the DataSet
+DataSet = sort(DataSet);
+N = length(DataSet);
+
+%Calculating the moments
+Mean = sum(DataSet)/N;
+Moment2 = sum(DataSet .^2)/N;
+Moment3 = sum(DataSet .^3)/N;
+Moment4 = sum(DataSet .^4)/N;
+
+%Calculating the centralized moments
+Var = sum((DataSet-Mean) .^2) / N;
+CentMomt3 = sum((DataSet-Mean) .^3) / N;
+CentMomt4 = sum((DataSet-Mean) .^4) / N;
+
+Standard_Dev = sqrt(Var);
+
+%Skewness is the third standardized moment
+Skew = sum(((DataSet-Mean)./Standard_Dev).^3)/N;
+SMom4 = sum(((DataSet-Mean)./Standard_Dev).^4)/N;
+
+Coef_Var = Standard_Dev/Mean;
+
+Exc_Kurt = SMom4 - 3;
+
+
