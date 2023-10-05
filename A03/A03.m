0f30506b8a8d57ae38e3a98f44c38eb5e8bd4ff7
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-10-05 18:23:43 +0200

    Reading DataSet

---
+++ A03/A03.m
@@ -0,0 +1,10 @@
+% Marcos Vinicius Firmino Pietrucci
+% Assigment 3
+
+%Reading the Traces
+    %Just like in class, will add it to a 3 column matrix
+fl1 = csvread('Trace1.csv');
+fl2 = csvread('Trace2.csv');
+fl3 = csvread('Trace3.csv');
+
+DataSet = [fl1, fl2, fl2];
