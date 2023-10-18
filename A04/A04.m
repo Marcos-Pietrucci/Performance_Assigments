5dca9f1e37cd59ba72f10a635237a394d1b9ef5a
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-10-18 22:24:43 +0200

    A04: Fixed weibull bug + added data presentation

--- A04/A04.m
+++ A04/A04.m
@@ -3,7 +3,7 @@
 % Assigment 4
 
 %Reading the Traces
-DataSet = csvread('Trace1.csv');
+DataSet = csvread('Trace2.csv');
 % DataSet = csvread('Trace2.csv');
 
 %Creating the DataSet
@@ -25,18 +25,18 @@ right_boundary = Mean + sqrt(12*(Moment2 - Mean^2))/2;
 exp_lambda = 1/Mean;
 
 %Fitting the Erlang distribution
-k = round(1/coef_var^2);
-lambda_erlang = k/Mean;
+k_erlang = round(1/coef_var^2);
+lambda_erlang = k_erlang/Mean;
 
-%Fitting the Weibull
-syms scale shape
-eq1 = Mean == scale * gamma(1 + 1/shape); %Defiing first moment equation
-eq2 = Moment2 == (scale^2) * gamma(1 + 2/shape); %Second moment
-weibull_params = vpasolve([eq1, eq2], [scale, shape], [1,1]);
-weib_scale = double(weibull_params.scale);
-weib_shape = double(weibull_params.shape);
+%%%%% Applying method of moments %%%%%
 
-%Fitting the Pareto
+%Weibull
+weibull_equations = @(x) [x(1)*gamma(1 + 1/x(2))-Mean,x(1)^2 * gamma(1+2/x(2))-Moment2]; 
+results = fsolve(weibull_equations, [1,1]);
+weib_scale = results(1);
+weib_shape = results(2);
+
+%Pareto
 syms alpha m
 eq3 = Mean == (alpha * m) / (alpha - 1); %Equations from slides
 eq4 = Moment2 == (alpha * m^2)/(alpha-2);
@@ -45,17 +45,19 @@ alpha_pareto = double(pareto_params.alpha(1));
 m_pareto = double(pareto_params.m(1));
 
 %Hypo-Exponential using MLE method
-lambda1 = 1/(0.3*Mean);
-lambda2 = 1/(0.7*Mean);
-hypo_parameters = mle(DataSet, 'pdf', @(DataSet, lambda1, lambda2)HypoExp_pdf(DataSet,[lambda1, lambda2]), ...
-  'Start', [lambda1,lambda2]);
+lambda1_hypo = 1/(0.3*Mean);
+lambda2_hypo = 1/(0.7*Mean);
+hypo_parameters = mle(sDataSet, 'pdf', @(DataSet, lambda1_hypo, lambda2_hypo) ...
+    HypoExp_pdf(DataSet,[lambda1_hypo, lambda2_hypo]), ...
+  'Start', [lambda1_hypo,lambda2_hypo]);
 
 %Hyper_Exponential using MLE method
 p1 = 0.4;
-lambda1 = 0.8/Mean;
-lambda2 = 1.2/Mean;
-hyper_parameters = mle(DataSet, 'pdf', @(DataSet, lambda1, lambda2, p1)HyperExp_pdf(DataSet,[lambda1, lambda2, p1]), ...
-    'Start', [lambda1,lambda2, p1]);
+lambda1_hyper = 0.8/Mean;
+lambda2_hyper = 1.2/Mean;
+hyper_parameters = mle(sDataSet, 'pdf', @(DataSet, lambda1_hyper, lambda2_hyper, p1) ...
+    HyperExp_pdf(DataSet,[lambda1_hyper, lambda2_hyper, p1]), ...
+    'Start', [lambda1_hyper,lambda2_hyper, p1]);
 
 %%%%%%%%%%%%%%% PLOTING RESULTS %%%%%%%%%%%%%%%
 
@@ -64,8 +66,8 @@ if coef_var < 1 %Only available if the Cv is less than 1
     figure(1)
     plot(sDataSet, [1:N]/N, ".", t, Unif_cdf(t, [left_boundary, right_boundary]), ...
         t, Exp_cdf(t, [exp_lambda]), ...
-        t, gamcdf(t,k, 1/lambda_erlang), ...
-        t, wblcdf(t, weib_scale, weib_shape), ...
+        t, gamcdf(t, k_erlang, 1/lambda_erlang), ...
+        t, Weibull_cdf(t, [weib_scale, weib_shape]), ...
         t, HypoExp_cdf(t, [hypo_parameters]), ...
         t, Pareto_cdf(t, [alpha_pareto, m_pareto]))
     legend({'DataSet','Uniform', 'Exponential','Erlang', 'Weibull', 'HypoExponential', 'Pareto'},'Location','southeast')
@@ -78,8 +80,8 @@ if coef_var > 1 %Only available if the Cv is less than 1
     figure(1)
     plot(sDataSet, [1:N]/N, ".", t, Unif_cdf(t, [left_boundary, right_boundary]), ...
         t, Exp_cdf(t, [exp_lambda]), ...
-        t, gamcdf(t, k, 1/lambda_erlang), ...
-        t, wblcdf(t, 5.75773, 2.12057), ...
+        t, gamcdf(t, k_erlang, lambda_erlang), ...
+        t, Weibull_cdf(t, [weib_scale, weib_shape]), ...
         t, HyperExp_cdf(t, [hyper_parameters]), ...
         t, Pareto_cdf(t, [alpha_pareto, m_pareto]))
         
@@ -88,6 +90,35 @@ if coef_var > 1 %Only available if the Cv is less than 1
     grid
 end
 
+fprintf("\n Results:");
+fprintf("\nMean: %.5f", Mean);
+fprintf("\nMoment2: %.5f", Moment2);
+fprintf("\nMoment3: %.5f", Moment3);
+fprintf("\nLeft-bound(a): %.5f", left_boundary);
+fprintf("\nRight-bound(a): %.5f", right_boundary);
+fprintf("\nExponential rate (lambda): %.5f", exp_lambda);
+fprintf("\nErlang k: %.5f", k_erlang);
+fprintf("\nErlang lambda: %.5f", lambda_erlang);
+fprintf("\nWeibull shape k: %.5f", weib_shape);
+fprintf("\nWeibull scale lambda: %.5f", weib_scale);
+fprintf("\nPareto shape alpha: %.5f", alpha_pareto);
+fprintf("\nPareto scale m: %.5f", m_pareto);
+fprintf("\nHyper-Exponential parameters: %.5f %.5f %.5f", hyper_parameters(1), hyper_parameters(2), hyper_parameters(3));
+fprintf("\nHypo-Exponential parameters: %.5f %.5f", hypo_parameters(1), hypo_parameters(2));
+fprintf("\n");
+
+
+
+function F = Weibull_cdf(x, p)
+    lambda  = p(1);
+	k = p(2);
+	i = 1;
+
+    while i ~= length(x)  + 1
+	    F(i) = 1 - exp(-x(1)/lambda)^k;
+        i = i+1;
+    end
+end
 
 function F = Pareto_cdf(x, p)
 	alpha = p(1); %alpha value (shape)
