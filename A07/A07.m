244ab67852a180700d8a595876773a65666950df
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-10-28 17:18:36 +0200

    A07: The main state machine is done

--- A07/A07.m
+++ A07/A07.m
@@ -1,44 +1,54 @@
 % Marcos Vinicius Firmino Pietrucci
 % 10914211
-% Assigment 5
+% Assigment 7
 
 clear all;
 
 s = 1;
 t = 0;
 
-Tmax = 1000000;
+Tmax = 100000;
 
 trace = [t, s];
 
-Tentrance = 0;
-Tlava = 0; %s = 5
 Tc1 = 0; %s = 2
 Tc2 = 0; %s= 3
 Texit = 0; %s = 4
+Tlava = 0; %s = 5
 
-roundTripTime = 0;
+%To compute the average game time
 RTT = [];
-roundIndex = 1;
+game_time = 0;
+
+%To compute the win-ratio
+win_counter = 0;
+lose_counter = 0;
 
 while t < Tmax
+
+    %Starting the game
     if s == 1
-        if rand() < 0.3 %going to c2
-            %Computar aqui a chance de cair
+        if rand() < 0.3 %going to c2 via blue
             if rand() < 0.3 %Reached C2
                 %Time uniform a=3 b=6
-                ns = 3;
-            else %Will fall
-                %Time exp = 0.25
-                ns = 5;
+                dt = 3 + rand() * (6-3);
+                ns = 3; %C2
+            else % Fell in the Lava
+                %Time exp lambda = 0.25
+                dt = -log(rand())/0.25;
+                ns = 5; %Lava
             end
-        else %going to c1
-            %Computar aqui a chance de cair
-            if rand() <= 0.2
-                %Will fall with exp 0.5
+        else 
+            %Going to c1
+            if rand() < 0.2
+                %Fell in the Lava
+                % Exp lambd = 0.5
+                dt = -log(rand())/0.5;
                 ns = 5; %Lava
             else
-                %will not fall and reach C1 in Erlang k=4 lambd =1.5
+                %Success!
+                %Erlang k=4 lambd =1.5
+                dt = -log(prod(rand(4,1)))/1.5;
                 ns = 2; %C1
             end
         end
@@ -47,23 +57,28 @@ while t < Tmax
     if s == 2 %C1 successfully reached     
         if rand() < 0.50
             %Will continue on yellow to C2
-            if rand () < 025
+            if rand () < 0.25
                 %Was successfull, reached C2
                 %Erlang k=3 lambd =2
+                dt = -log(prod(rand(3,1)))/2;
                 ns = 3;
             else
-                %Was not successfull
+                %Fell in the Lava
                 %Exponential 0.4
+                dt = -log(rand())/0.4;
+                ns = 5;
             end
         else
             %Will go to C2 via white
             if rand() < 0.6
                 %Was successfull!
                 %Exp = 0.15
+                dt = -log(rand())/0.15;
                 ns = 3;
             else
                 %Fell in the lava
-                %exp lambd = 0.2
+                %Exp lambd = 0.2
+                dt = -log(rand())/0.2;
                 ns = 5;
             end
         end		
@@ -73,6 +88,7 @@ while t < Tmax
     if s == 3
         %Will try to go to the exit
         %Both cases have Erlang k= 5 lambd = 4
+        dt = -log(prod(rand(5,1)))/4;
         if rand() < 0.6
             %Success
             ns = 4;
@@ -82,26 +98,25 @@ while t < Tmax
         end
     end
     
-    %Reached the end! Need 5min to reset the room
+    %Reached the end! 
     if s == 4
-        dt = 60*5;
+        dt = 5; %Need 5min to reset the room
+        ns = 1;
+        win_counter = win_counter + 1;
     end
     
     %Fell in Lava... Need 5min to reset the rooms
     if s == 5
-        dt = 60*5;
+        dt = 5; %Need 5min to reset the room
+        ns = 1;
+        lose_counter = lose_counter + 1;
     end
 
     s = ns;
 	t = t + dt;
 	trace(end + 1, :) = [t, s];
-
 end
 
-Ps1 = Tc1 / t
-Ps2 = Tc2 / t
-Ps3 = Texit / t
-
-
-
+game_counter = win_counter + lose_counter;
+win_prob = win_counter/game_counter
 
