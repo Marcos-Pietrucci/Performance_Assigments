c0de910da6077ae3fb44c37f523234cb53955181
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-11-07 20:45:11 +0100

    A09: Average power consumption computed

--- A09/A09.m
+++ A09/A09.m
@@ -4,9 +4,15 @@
 
 %Defining the exponential rates in minutes
 tscan = 2;
-tnigh = 18;  
+tnight = 18;  
 tsun = 3;
 tcloudy = 8;  %Related to Down states
+
+%Given the times we can calculate the rates!
+scan_r = 1/tscan;
+night_r = 1/tnight;
+sun_r = 1/tsun;
+cloudy_r = 1/tcloudy;
 
 %Defining the weather probability
 prob_night = 0.5;
@@ -15,14 +21,28 @@ prob_cloud = 0.1637;
 
 %Generating the states vectors
 %The order: SCAN-NIGHT-SUNNY-CLOUDY
-SCAN = [0, tscan*prob_night, tscan*prob_sun, tscan*prob_cloud];
+SCAN = [0, scan_r*prob_night, scan_r*prob_sun, scan_r*prob_cloud];
 q1 = -sum(SCAN(2:4));
 SCAN(1) = q1;
 
-NIGHT = [tnigh, -tnigh, 0, 0];
-SUNNY = [tsun, 0,-tsun, 0];
-CLOUD = [tcloudy, 0, 0, -tcloudy];
+NIGHT = [night_r, -night_r, 0, 0];
+SUNNY = [sun_r, 0,-sun_r, 0];
+CLOUD = [cloudy_r, 0, 0, -cloudy_r];
 
 
 %Creating the infentesimal generator
 Q = [SCAN;NIGHT;SUNNY;CLOUD];
+
+%Calculating the steady state probability of the system
+Qp = [Q(:, 1:3), ones(4,1)];
+
+pi_lim = [0,0,0,1] *inv(Qp);
+
+%Reward vector related to power consumption
+alpha_power = [12, 0.1, 0.1, 0.1];
+avg_powCom = pi_lim *alpha_power';
+
+%Reward matrix related to power consumption
+%The order: SCAN-NIGHT-SUNNY-CLOUDY
+reward_m = []
+
