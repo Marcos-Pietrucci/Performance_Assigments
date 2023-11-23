026a0407fb12bbc021b417020fee6b70ad9265c5
Marcos Pietrucci <marcospietrucci@gmail.com>
2023-11-23 18:32:34 +0100

    A11: M/M/1/K calculations and presentation done

--- A11/A11.m
+++ A11/A11.m
@@ -4,6 +4,32 @@
 
 %Given values
 lambda_1 = 150 / 60;
-service_time=350/1000;
+serv_time=350/1000;
 k=32;
+
+%Utilization
+rho=lambda_1*serv_time;
+U = (rho - rho^(k+1))/(1-rho^(k+1));
+
+%Calculating requested values
+loss_p = (rho^k - rho^(k+1))/(1-rho^(k+1));
+avg_number_of_jobs = (rho/(1-rho)) - ((k+1) * rho^(k+1))/(1-rho^(k+1));
+drop_rate = lambda_1 * (rho^k - rho^(k+1))/(1-rho^(k+1));
+avg_resp_time = serv_time * (1-(k+1) * rho^k + k * rho^(k+1))/((1-rho)*(1-rho^k));
+avg_time_in_queue= rho * avg_number_of_jobs / lambda_1;
+
+
+%Presenting values for the  M/M/1/K
+frprintf("\n\nM/M/1/K Values: ");
+fprintf("\nUtilization: %.4f", U);
+fprintf("\nLoss probability: %.4f", loss_p);
+fprintf("\nAverage number of jobs in the system: %.4f", avg_number_of_jobs);
+fprintf("\nDrop rate: %.4f", drop_rate);
+fprintf("\nAverage response time: %.4f", avg_resp_time);
+fprintf("\nAverage time spent in the queue: %.4f", avg_time_in_queue);
+
+%%%%%%%%%%%% M/M/2/K %%%%%%%%%%%%
+
+
+
 
